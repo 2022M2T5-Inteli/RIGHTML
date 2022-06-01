227e565f88593e65622988a641eef5ceaafaf56d
vict0rcarvalh0 <victordecarvalho@outlook.pt>
2022-06-01 16:10:24 -0300

    logn/cadastro novo refatorado

--- WEBSITE/frontend/login/login.js
+++ WEBSITE/frontend/login/login.js
@@ -1,11 +1,3 @@
 function login() {
-    var loginInput = document.getElementById('loginInput')
-
-    if (loginInput.textContent == "admin@falconi.com") {
-        window.location.replace("../adminDashboard/adminDashboard.html");
-    }
-
-    else if (loginInput.textContent == "gestor@gmail.com") {
-        window.location.replace("../schoolManagerDashboard/schoolManagerDashboard.html");
-    }
+    window.location = "../homePage/home.html"
 }
