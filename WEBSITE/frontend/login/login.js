e2289b2f85d4e96bf28f4cbac4e4917e15eaed77
0rdep12 <78965293+0rdep12@users.noreply.github.com>
2022-06-01 16:21:54 -0300

    Revert "head"
    This reverts commit 67264b4f85d46a3fc3bddb8bd90c75562ed58712, reversing
    changes made to 9ab6f854600631e865256e48cd936b31efd3dcd0.

--- WEBSITE/frontend/login/login.js
+++ WEBSITE/frontend/login/login.js
@@ -1,3 +1,11 @@
 function login() {
-    window.location = "../homePage/home.html"
+    var loginInput = document.getElementById('loginInput')
+
+    if (loginInput.textContent == "admin@falconi.com") {
+        window.location.replace("../adminDashboard/adminDashboard.html");
+    }
+
+    else if (loginInput.textContent == "gestor@gmail.com") {
+        window.location.replace("../schoolManagerDashboard/schoolManagerDashboard.html");
+    }
 }
