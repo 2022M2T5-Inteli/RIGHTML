e553b5565ea573f29aaa3de5e7c0ce4726032726
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-06-09 09:33:29 -0300

    UPDATING
    Ouçam little dark age

--- WEBSITE/frontend/questionnaire/equityAxis.js
+++ WEBSITE/frontend/questionnaire/equityAxis.js
@@ -15,8 +15,18 @@ function readQuestionsFromDatabase() {
                             
                         <div class="question-wording">
                             <p>${element['text']}</p>
-                        </div>`
-                    
+                        </div>`;
+                        let alternatives = getAlternatives(element['id']);
+                        console.log("Meu enunciado é: " + element['text'] + "e minhas opções são: " + alternatives)
+                        alternatives.forEach(alternative => {
+                            questionsContainer.innerHTML +=
+                    `<div class="form-check">
+                    <input class="form-check-input" type="radio" name="question${element['position']}" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">${alternative}</label>
+                    </div>`
+                  
+                        
+                        })
               
                 });
         
@@ -26,5 +36,60 @@ function readQuestionsFromDatabase() {
     });
 }
 
+let alternatives = [];
 
+function getAlternatives(question_id) {
+    alternatives = [];
+    $.ajax({
+        url: "http://127.0.0.1:3001/options",
+        type: 'GET',
+        async: false,
+        success: data => { 
+            data.forEach(element => {
+                if (parseInt(question_id) === parseInt(element['question_id'])) {
+                    alternatives.push(element['text']);
+                }
+            })
+        }
 
+    })
+    return alternatives;
+}
+
+// Essa função serve para inputar as questões respondidas no banco de dados  
+$(document).ready(function() {
+    $("#update_data").click(function () {
+        let url = "http://127.0.0.1:3001/answerinsert";
+        
+        console.log("works")
+        
+        {  
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: {
+                    cnpj: $('#CNPJ_key').val(),
+                    number_of_employees: $('#update_employees_number').val(),
+                    number_of_students: $('#update_students_number').val(),
+
+                },
+            })
+        }});
+    });
+
+    function saveAnswers(question_id) {
+        alternatives = [];
+        $.ajax({
+            url: "http://127.0.0.1:3001/answerinsert",
+            type: 'POST',
+            success: data => { 
+                data.forEach(element => {
+                    if (parseInt(question_id) === parseInt(element['question_id'])) {
+                        alternatives.push(element['text']);
+                    }
+                })
+            }
+    
+        })
+        return alternatives;
+    }
