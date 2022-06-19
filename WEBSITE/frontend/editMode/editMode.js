5d02a6b2a491149fb7e3cb0a7feb815ad6e8d6d7
elisaflemer <elisa.flemer@sou.inteli.edu.br>
2022-06-18 22:41:16 -0300

    trying to remove question number

--- WEBSITE/frontend/editMode/editMode.js
+++ WEBSITE/frontend/editMode/editMode.js
@@ -346,29 +346,26 @@ $('#add-axis').on('click', function (event) {
 });
 
 function deleteAxis(axis_id) {
-    if (confirm("Tem certeza que deseja deletar este eixo? Todos os subeixos e questões associados também serão deletados.")) {
-        if ($('#axis-dropdown').val() != '') {
-            let questions = getAllQuestionsFromAxis(axis_id);
-            questions.forEach(question => {
-                deleteQuestion(question['id']);
-            });
-            let subaxes = getSubdivisionsFromAxisId(axis_id);
-            subaxes.forEach(subaxis => {
-                deleteSubaxis(subaxis['id'])
-            })
-            $.ajax({
-                url: "http://127.0.0.1:3001/axisdelete",
-                type: 'POST',
-                async: false,
-                data: {
-                    id: axis_id
-                }
-            });
+    if ($('#axis-dropdown').val() != '') {
+        let questions = getAllQuestionsFromAxis(axis_id);
+        questions.forEach(question => {
+            deleteQuestion(question['id']);
+        });
+        let subaxes = getSubdivisionsFromAxisId(axis_id);
+        subaxes.forEach(subaxis => {
+            deleteSubaxis(subaxis['id'])
+        })
+        $.ajax({
+            url: "http://127.0.0.1:3001/axisdelete",
+            type: 'POST',
+            async: false,
+            data: {
+                id: axis_id
+            }
+        });
 
 
-        }
     }
-
 }
 
 
@@ -741,31 +738,28 @@ function showQuestionsByAxis() {
     axes.forEach(axis => {
         let questions = getAllQuestionsFromAxis(axis['id'])
         questions.forEach(question => {
-            document.getElementById(`${axis['name']}-body`).innerHTML += `<div id="question${question['position']}">
-            <div class="row question-header">
+            document.getElementById(`${axis['name']}-body`).innerHTML += `
+            <div id="question${question['position']}">
                 <div class="col-sm-9">
-                    <h5>Questão ${question['position']}</h5>
+                    <p>${question['text']}</p>
                 </div>
                 <div class="col-sm-3">
-                <span>
-                    <button type="button" id="trash${question['id']}" class="btn btn-light" onclick="deleteQuestion(${question['id']})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-                        </svg>
-                    </button>
-                    <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#editModal" onclick="updateEditModal(${question['id']})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                        </svg>
-                    </button>
+                    <span>
+                        <button type="button" id="trash${question['id']}" class="btn btn-light" onclick="deleteQuestion(${question['id']})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                            </svg>
+                        </button>
+                        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#editModal" onclick="updateEditModal(${question['id']})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                            </svg>
+                        </button>
                     </span>
                 </div>
-                
-            </div>
-            <div class="question-wording">
-                <p>${question['text']}</p>
-            </div>`
+            
+            `
             let alternatives = getAlternatives(question['id']);
             alternatives.forEach(alternative => {
                 document.getElementById(`${axis['name']}-body`).innerHTML +=
@@ -780,22 +774,23 @@ function showQuestionsByAxis() {
 };
 
 function questionsExist() {
+    let questionsExist = false;
     $.ajax({
         url: "http://127.0.0.1:3001/questions",
         type: 'GET',
         async: false,
         success: data => {
             if (data.length > 0) {
-                return true;
+                questionsExist = true;
             }
-            return false;
         }
 
     })
+    return questionsExist
 }
 
 function readQuestionsFromDatabase() {
-    if (questionsExist) {
+    if (questionsExist()) {
         document.getElementById("questions-container").innerHTML = '';
         createAxisAccordions("questions-container");
         showQuestionsByAxis();
