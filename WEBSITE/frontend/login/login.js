4df764540473ab1d9e34cf1a82ace6317468adb0
elisaflemer <elisa.flemer@sou.inteli.edu.br>
2022-06-19 19:15:05 -0300

    logins working

--- WEBSITE/frontend/login/login.js
+++ WEBSITE/frontend/login/login.js
@@ -3,11 +3,29 @@ function login() {
     var userType = document.querySelector('input[type=radio][name=userType]:checked').value;
     if (userType === "schoolManager") {
         if(checkSchoolManager(loginInput)) {
+            localStorage.setItem("loggedIn", "true");
             localStorage.setItem("primaryKey", loginInput);
             localStorage.setItem("table", "school_manager");
             window.location.href = "../schoolManagerDashboard/schoolManagerDashboard.html";
         } else {
             alert("Este CPF não está cadastrado como gestor escolar.")
+        }
+    } else if (userType === "networkManager") {
+        if(checkNetworkManager(loginInput)) {
+            localStorage.setItem("loggedIn", "true");
+            localStorage.setItem("primaryKey", loginInput);
+            localStorage.setItem("table", "network_manager");
+        } else {
+            alert("Este CPF não está cadastrado como gestor de rede.")
+        }
+    } else if (userType === "falconi") {
+        if(checkFalconiEmployee(loginInput)) {
+            localStorage.setItem("loggedIn", "true");
+            localStorage.setItem("primaryKey", loginInput);
+            localStorage.setItem("table", "employee");
+            window.location.href = "../adminDashboard/adminDashboard.html";
+        } else {
+            alert("Este CPF não está cadastrado como funcionário Falconi.")
         }
     }
     console.log(loginInput + " " + userType)
@@ -23,6 +41,41 @@ function checkSchoolManager(loginInput) {
             data.forEach(schoolManager => {
                 if(schoolManager['cpf'] === loginInput) {
                     user = schoolManager;
+                }
+            })
+        }
+    })
+    return user;
+}
+
+function checkNetworkManager(loginInput) {
+    let user = null;
+    console.log(loginInput)
+    $.ajax({
+        url: "http://127.0.0.1:3001/networkmanagers",
+        type: 'GET',
+        async: false,
+        success: data => {
+            data.forEach(networkManager => {
+                if(networkManager['cpf'] === loginInput) {
+                    user = networkManager;
+                }
+            })
+        }
+    })
+    return user;
+}
+
+function checkFalconiEmployee(loginInput) {
+    let user = null;
+    $.ajax({
+        url: "http://127.0.0.1:3001/employees",
+        type: 'GET',
+        async: false,
+        success: data => {
+            data.forEach(falconiEmployee => {
+                if(falconiEmployee['cpf'] === loginInput) {
+                    user = falconiEmployee;
                 }
             })
         }
