3c836251e3382776c0f639daf8638a2b410aa18d
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-06-22 23:44:54 -0300

    school manager sign up almost fixed

--- WEBSITE/frontend/signUpSchoolManager/signUpSchoolManager.js
+++ WEBSITE/frontend/signUpSchoolManager/signUpSchoolManager.js
@@ -43,19 +43,20 @@ function get_network_id() {
 $(document).ready(function () {
     $("#criarConta").click(function () {
         let url = "http://127.0.0.1:3001/schoolinsert";
-        get_network_id();
+       // get_network_id();
         get_cnpjs();
-        console.log(typeof parseInt($('#network_id').val()))
-        console.log(typeof school_id[0]);
-        console.log(school_id.includes(parseInt($('#network_id').val())))
+        // console.log(typeof parseInt($('#network_id').val()))
+        // console.log(typeof school_id[0]);
+        // console.log(school_id.includes(parseInt($('#network_id').val())))
 
         if ($('#type').val() === "" ||
             $('#employee_number').val() === "" ||
             $('#CNPJ').val() === "" ||
             $('#student_number').val() === "" ||
             $('#school_name').val() === "" ||
-            $('#school_id').val() === "" ||
-            $('#network_id').val() === "") {
+            $('#school_id').val() === "" )
+            //$('#network_id').val() === "") 
+            {
             // alert("preencha todos os dados")
             Swal.fire({
                 icon: 'error',
@@ -63,14 +64,6 @@ $(document).ready(function () {
             })
         }
 
-
-        else if ((!school_id.includes(parseInt($('#network_id').val()))) || (CNPJS.includes(parseInt($('#CNPJ').val())))) {
-            // alert("Preencha os dados corretamente")
-            Swal.fire({
-                icon: 'error',
-                title: 'Preencha os dados corretamente!',
-            })
-        }
 
         else {
             Swal.fire({
@@ -85,13 +78,13 @@ $(document).ready(function () {
                     url: url,
                     type: 'POST',
                     data: {
-                        type_of_institution: $('#type').val(),
+                        type: $('input[name=opcao]:checked').val(),
                         number_of_employees: $('#employee_number').val(),
                         cnpj: $('#CNPJ').val(),
                         number_of_students: $('#student_number').val(),
                         name: $('#school_name').val(),
                         school_census_id: $('#school_id').val(),
-                        network_id: $('#network_id').val()
+                        //network_id: $('#network_id').val()
                     },
                 })
             };
